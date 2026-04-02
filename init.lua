91144be7e335cd6f896745bb77ebbe22b8ea9ac7
sunnypek <sunnypek@xiaomi.com>
2026-04-02 15:11:19 +0800

    feat: use win32yank as clipboard

--- init.lua
+++ init.lua
@@ -444,3 +444,16 @@ require("nvim-tree").setup({
 		},
 	},
 })
+
+vim.g.clipboard = {
+	name = "win32yank-wsl",
+	copy = {
+		["+"] = "win32yank.exe -i --crlf",
+		["*"] = "win32yank.exe -i --crlf",
+	},
+	paste = {
+		["+"] = "win32yank.exe -i --lf",
+		["*"] = "win32yank.exe -i --lf",
+	},
+	cache_enabled = 0,
+}
